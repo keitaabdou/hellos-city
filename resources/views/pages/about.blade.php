b04ea23c45a935db8d2d4a4aa274fd8395117926
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-16 17:05:08 +0000

    changement d'image

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,11 +2,11 @@
 
 @section('title', 'About Us  ' . config('base.name'))
 
-<img src="/images/tdn-logo.png" alt="TDN  Logo" class="my-12 rounded-full shadow-md">
+<img src="/images/image_keita.JPG" alt="TDN  Logo" class="my-12 rounded-full shadow-md" style="width: 200px">
 
 @section('content')
 
-            <h2 class="mb-5 text-gray-700">Build white <span class=" text-pink-500">&hearts;</span> By TEACHER DU NET.</h2>
+            <h2 class="mb-5 text-gray-700">Build white <span class=" text-pink-500">&hearts;</span> By KEITA ABDOU KARIM.</h2>
             <P><a href="/" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'acceuil</a></P>
 
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,11 +2,11 @@
 
 @section('title', 'Hello-city')
 
-    <img src="/images/quebec-flag.png" alt="Quebec flag" class="mt-12 rounded shadow-md" style="height: 150px; width:200px;">
+    <img src="/images/drapeau.png" alt="Quebec flag" class="mt-12 rounded shadow-md" style="height: 150px; width:200px;">
 
 @section('content')
-            <h1 class="mt-5 text-3xl  sm:text-5xl font-semibold text-indigo-600">Hello From Quebec !</h1>
+            <h1 class="mt-5 text-3xl  sm:text-5xl font-semibold text-indigo-600">Hello From Abidjan !</h1>
 
-            <p class="text-lg text-gray-800">It's currenty {{ date('h:i A')}}.</p>     
+            <p class="text-lg text-gray-800">It's currenty {{ date('h:i A')}}.</p>
 
 @endsection
